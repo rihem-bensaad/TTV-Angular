2ce509fabb2f784cd2aee80817ceecdccf676f41
Rihem <rihambensaad@gmail.com>
2022-09-25 19:49:22 +0100

    télétravailproject

---
+++ client/src/app/components/fogotpass/fogotpass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fogotpass',
+  templateUrl: './fogotpass.component.html',
+  styleUrls: ['./fogotpass.component.css']
+})
+export class FogotpassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
